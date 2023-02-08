389518804807b7f632c21bbb98627e9879f058ba
umeda tohru <umeda@sun.cims.jp>
2023-02-09 07:41:29 +0900

    テンプレートエンジンを使用

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 crud = Blueprint("crud", __name__, template_folder="templates", static_folder="static")
 
 
 @crud.route("/")
 def index():
-    return "Hello from Flask!"
+    return render_template("crud/index.html")
